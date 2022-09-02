e67b2f12ef4d7e8ada2bf8ed213140e938162b59
Yqat <112522876+Yqat@users.noreply.github.com>
2022-09-02 17:04:16 +0300

    Add files via upload

---
+++ less 2.py
@@ -0,0 +1,23 @@
+# my_name= "vlad"
+# day= "02.09.2022"
+# print(f"Good day {my_name}! {day} is a perfect day to learn some python.")
+
+# first_name = "Roman"
+# last_name = "Yakobchuk"
+# print("My name" + " " + first_name + " " + "and my last name is " + last_name)
+
+
+
+a=5
+b=10
+
+res_add= a+b
+res_sub= a-b
+res_divis= a/b
+res_pow= a**b
+res_mod= a%b
+res_floor= a//b
+
+# print(res_add,res_sub,res_divis,res_pow,res_mod,res_floor,)
+
+
